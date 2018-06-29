587fb66558ac9c4a0f084f180f225c9cbcbe767a
Aluo <green.rabite@gmail.com>
2018-06-29 14:47:45 -0700

    Initial COmmit

---
+++ examples/chat/cresta-challenge/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Dashboard from './components/dashboard';
+
+class App extends Component {
+  render() {
+    return (
+      <Dashboard/>
+    );
+  }
+}
+
+export default App;
---
+++ examples/chat/cresta-challenge/src/components/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Dashboard extends React.Component{
+  render(){
+    return(
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default Dashboard;
